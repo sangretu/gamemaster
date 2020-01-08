c326a7b7810d30d72a7598e32673e15ba2489b1b
sangretu <sangretu@gmail.com>
2020-01-08 02:58:58 -0800

    step 3

--- gamemaster evolution/src/js/evolution.js
+++ gamemaster evolution/src/js/evolution.js
@@ -9,7 +9,7 @@
 console.log('Game Master Evolution');
 
 /*
- step two. Add round markers. Between each set of attacks, a divider line is shown along with the current round number. Final read out has the number of rounds append it, for example, “the goblin one in three rounds. 
+ step three.  allow the player to select an action for each round. The two actions the player may select “attack“ and “flea”. The player receives a simple prompt  on the consul, and inputs their selected option as a function call in the consul.  if the player fleas, the battle ends prematurely with the final output, “flat“.  amendment: “you flag after X rounds of combat“. 
 */
 
 var goblin_hit_points = 4;
@@ -21,9 +21,8 @@ var roll_to_hit = function()
   return Math.floor(Math.random()*20);
 };
 
-while (goblin_hit_points > 0 && player_hit_points > 0)
+var attack = function()
 {
-
   current_round++;
   console.log('------------ round ' + current_round + ' ------------');
 
@@ -36,20 +35,23 @@ while (goblin_hit_points > 0 && player_hit_points > 0)
     console.log('you miss the goblin');
   };
   
-  if (!(goblin_hit_points > 0)) break;
+  if (goblin_hit_points > 0)
+  {  
+    if (roll_to_hit() >= 10)
+    {
+      console.log('the goblin hit you');
+      player_hit_points--;
+    } else
+    {
+      console.log('the goblin misses you');
+    };
+  }
   
-  if (roll_to_hit() >= 10)
-  {
-    console.log('the goblin hit you');
-    player_hit_points--;
-  } else
-  {
-    console.log('the goblin misses you');
-  };
-  
+  if (goblin_hit_points == 0) console.log('you win in ' + current_round + ' rounds');
+  else if (player_hit_points == 0) console.log('goblin wins in ' + current_round + ' rounds');
 };
 
-if (goblin_hit_points == 0) console.log('you win in ' + current_round + ' rounds');
-else if (player_hit_points == 0) console.log('goblin wins in ' + current_round + ' rounds');
-else console.log('error');
-
+var flee = function()
+{
+  console.log('you fled after ' + current_round + ' rounds of combat');
+};
