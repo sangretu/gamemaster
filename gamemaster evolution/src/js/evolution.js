3591bb6143049e470bd1a96ca3e3c7265dd059b9
sangretu <sangretu@gmail.com>
2020-01-08 06:52:38 -0800

    step 10

--- gamemaster evolution/src/js/evolution.js
+++ gamemaster evolution/src/js/evolution.js
@@ -9,69 +9,77 @@
 console.log('Game Master Evolution');
 
 /*
- step nine.  character classes.  The player is randomly assigned one of the standard set of character classes when the program begins.  each class, of the set listed in the players handbook to include the monk but not the Bard, has a different maximum hit die and a different armor class based on class armor restrictions.   when the player is assigned a class, before the battle begins, a line of output indicates what class they have been assigned, and what their head points armor class are.  One that was an accident   sorry, I just witnessed a traffic accident. Back to work. Each class also has a custom weapon damage range based on their weapon limitations. The standard stats provided earlier should be used for the fighter class unless it throws things out of balance. 
- 
- Note: also fixed bug in roll_die, max value was excluded
+ step 10.   refine hit odds. Assign the goblin an appropriate armor class from the monster manual. Assign minimum to hit values based on character class. Similarly for the goblin.  these should be static for now, but annotate the code to indicate the assumption of fighting this specific opponent. 
 */
 
 var character_classes =
 [
   {
     class_name : 'cleric',
+    thac_goblin: 14, // ac 6
     max_hp     : 8,
     max_damage : 6, // club, flail, hammer, mace, staff
     ac         : 4 // any + shield
   },
   {
     class_name : 'druid',
+    thac_goblin: 14, // ac 6
     max_hp     : 8,
     max_damage : 6, // club, dagger, dart, hammer, scimitar, sling, spear, staff
     ac         : 7 // leather + wooden shield
   },
   {
     class_name : 'fighter',
+    thac_goblin: 14, // ac 6
     max_hp     : 10,
     max_damage : 8, // any
     ac         : 4 // any + shield
   },
   {
     class_name : 'paladin',
+    thac_goblin: 14, // ac 6
     max_hp     : 10,
     max_damage : 8, // any
     ac         : 2 // any + shield
   },
   {
     class_name : 'ranger',
+    thac_goblin: 14, // ac 6
     max_hp     : 8,
     max_damage : 6, // any
     ac         : 6 // any + shield
   },
   {
     class_name : 'magic-user',
+    thac_goblin: 15, // ac 6
     max_hp     : 4,
     max_damage : 4, // dagger, dart, staff
     ac         : 10 // none
   },
   {
     class_name : 'illusionist',
+    thac_goblin: 15, // ac 6
     max_hp     : 4,
     max_damage : 4, // dagger, dart, staff
     ac         : 10 // none
   },
   {
     class_name : 'thief',
+    thac_goblin: 15, // ac 6
     max_hp     : 6,
     max_damage : 6, // club, dagger, dart, sling, sword
     ac         : 8 // leather
   },
   {
     class_name : 'assassin',
+    thac_goblin: 15, // ac 6
     max_hp     : 6,
     max_damage : 4, // any
     ac         : 7 // leather + shield
   },
   {
     class_name : 'monk',
+    thac_goblin: 14, // ac 6
     max_hp     : 4,
     max_damage : 6, // bo sticks, club, crossbow, dagger, hand axe, javelin, jo stick, pole arm, spear, staff
     ac         : 10 // none
@@ -87,22 +95,26 @@ var roll_die = function(max)
 
 var player_class = character_classes[roll_die(character_classes.length)-1];
 
-var goblin_max_hit_points = roll_die(4);
+var player_class_name     = player_class.class_name;
 var player_max_hit_points = roll_die(player_class.max_hp);
-var goblin_hit_points     = goblin_max_hit_points;
 var player_hit_points     = player_max_hit_points;
 var player_armor_class    = player_class.ac;
-var goblin_armor_class    = 8;
+var player_thac_goblin    = player_class.thac_goblin;
+
+var goblin_max_hit_points = roll_die(7);
+var goblin_hit_points     = goblin_max_hit_points;
+var goblin_armor_class    = 6;
+var goblin_thac0          = 21;
+
 var current_round         = 0;
 
 console.log
 (
-  'Player : '
-  + player_class.class_name // Note: This is class not character
-  + ', hp ' 
-  + player_max_hit_points 
-  + ', ac ' + player_armor_class
+  'Player : '       + player_class_name 
+  + ', hp '         + player_max_hit_points
+  + ', ac '         + player_armor_class
   + ', max damage ' + player_class.max_damage // Note: This is class not character
+  + ', thac6 '      + player_thac_goblin
 );
 
 var btn_attack;
@@ -138,7 +150,7 @@ var attack = function()
 
   if (player_hit_points > 0)
   {
-    if (roll_die(20) >= (20 - goblin_armor_class) )
+    if (roll_die(20) >= player_thac_goblin) // (20 - goblin_armor_class) )
     {
       var damage = roll_die(6);
       console.log('you hit the goblin for ' + damage + ' points of damage');
@@ -151,7 +163,7 @@ var attack = function()
   
   if (goblin_hit_points > 0)
   {  
-    if (roll_die(20) >= (20 - player_armor_class) )
+    if (roll_die(20) >= (goblin_thac0 - player_armor_class))// (20 - player_armor_class) )
     {
       var damage = roll_die(4);
       console.log('the goblin hit you for ' + damage + ' points of damage');
